0e3502af23a75823de8786b5757acd97e1c60918
Asheer Rizvi <asheer.vil@gmail.com>
2026-02-20 10:32:23 +0530

    build and coverage archives

--- src/examples/obstacle-course/exercise.test.tsx
+++ src/examples/obstacle-course/exercise.test.tsx
@@ -1,19 +1,65 @@
+import { fireEvent, screen } from '@testing-library/react';
+import { render } from 'test/utilities';
 import ObstacleCourse from '.';
 
-it.todo('should input text into the input field', () => {
+it('should input text into the input field', async () => {
+  const { user } = render(<ObstacleCourse />);
   const thought = 'Ravioli are a form of pop tart.';
+  const input = screen.getByTestId<HTMLInputElement>('text-input');
+  await user.type(input, thought);
+  expect(input).toHaveValue(thought);
 });
 
-it.todo('should control a select input', () => {});
+it('should control a select input', async () => {
+  const { user } = render(<ObstacleCourse />);
+  const select = screen.getByTestId<HTMLSelectElement>('select-input');
+  await user.selectOptions(select, 'Thor');
+  expect(select).toHaveValue('Thor');
+});
 
-it.todo('should find and control a checkbox input', () => {});
+it('should find and control a checkbox input', async () => {
+  const { user } = render(<ObstacleCourse />);
+  const checkbox = screen.getByTestId<HTMLInputElement>('checkbox-onion');
+  await user.click(checkbox);
+  expect(checkbox).toBeChecked();
+});
 
-it.todo('should find and control a radio input', () => {});
+it('should find and control a radio input', async () => {
+  const { user } = render(<ObstacleCourse />);
+  const radio = screen.getByTestId<HTMLInputElement>('radio-john');
+  await user.click(radio);
+  expect(radio).toBeChecked();
+});
 
-it.todo('should find and control a color input', () => {});
+it('should find and control a color input', () => {
+  render(<ObstacleCourse />);
+  const input = screen.getByTestId<HTMLInputElement>('color-input');
+  fireEvent.input(input, { target: { value: '#33cc99' } });
+  expect(input).toHaveValue('#33cc99');
+});
 
-it.todo('should find and control a date input', () => {});
+it('should find and control a date input', () => {
+  render(<ObstacleCourse />);
+  const input = screen.getByTestId<HTMLInputElement>('date-input');
+  fireEvent.input(input, { target: { value: '2022-01-01' } });
+  expect(input).toHaveValue('2022-01-01');
+});
 
-it.todo('should find and control a range input', () => {});
+it('should find and control a range input', () => {
+  render(<ObstacleCourse />);
+  const input = screen.getByTestId<HTMLInputElement>('range-input');
+  fireEvent.input(input, { target: { value: '7' } });
+  expect(input).toHaveValue('7');
+});
 
-it.todo('should find and control a file input', () => {});
+it('should find and control a file input', async () => {
+  const { user } = render(<ObstacleCourse />);
+  const input = screen.getByTestId<HTMLInputElement>('file-input');
+  const file = new File(['hello'], 'resume.pdf', { type: 'application/pdf' });
+  await user.upload(input, file);
+  expect(input.files).toHaveLength(1);
+  expect(input.files).toHaveLength(1);
+  expect(input.files?.[0].name).toBe('resume.pdf');
+  expect(input.files?.[0].type).toBe('application/pdf');
+  expect(input.files?.[0]).toStrictEqual(file);
+});
--- src/examples/packing-list/packing-list.test.tsx
+++ src/examples/packing-list/packing-list.test.tsx
@@ -1,5 +1,18 @@
-import { render, screen } from 'test/utilities';
-import PackingList from '.';
+import { PropsWithChildren } from 'react';
+import { Provider } from 'react-redux';
+import { render as baseRender, screen } from 'test/utilities';
+import { PackingList } from '.';
+import { createStore } from './store';
+
+const render: typeof baseRender = (Component, options) => {
+  const store = createStore();
+
+  const Wrapper = ({ children }: PropsWithChildren) => {
+    return <Provider store={store}>{children}</Provider>;
+  };
+
+  return baseRender(Component, { ...options, wrapper: Wrapper });
+};
 
 it('renders the Packing List application', () => {
   render(<PackingList />);
@@ -10,19 +23,75 @@ it('has the correct title', async () => {
   screen.getByText('Packing List');
 });
 
-it.todo('has an input field for a new item', () => {});
+it('has an input field for a new item', () => {
+  render(<PackingList />);
+  screen.getByRole('searchbox', {
+    name: /new item name/i,
+  });
+});
 
-it.todo(
-  'has a "Add New Item" button that is disabled when the input is empty',
-  () => {},
-);
+it('has a "Add New Item" button that is disabled when the input is empty', () => {
+  render(<PackingList />);
+  const newItemInput = screen.getByRole('searchbox', {
+    name: /new item name/i,
+  });
+  const addNewItemButton = screen.getByRole('button', {
+    name: /add new item/i,
+  });
+  expect(newItemInput).toHaveValue('');
+  expect(addNewItemButton).toBeDisabled();
+});
 
-it.todo(
-  'enables the "Add New Item" button when there is text in the input field',
-  async () => {},
-);
+it('enables the "Add New Item" button when there is text in the input field', async () => {
+  const { user } = render(<PackingList />);
+  const newItemInput = screen.getByRole('searchbox', {
+    name: /new item name/i,
+  });
+  const addNewItemButton = screen.getByRole('button', {
+    name: /add new item/i,
+  });
+  expect(addNewItemButton).toBeDisabled();
+  await user.type(newItemInput, 'Laptop');
+  expect(addNewItemButton).not.toBeDisabled();
+});
 
-it.todo(
-  'adds a new item to the unpacked item list when the clicking "Add New Item"',
-  async () => {},
-);
+it('adds a new item to the unpacked item list when the clicking "Add New Item"', async () => {
+  const { user } = render(<PackingList />);
+  const newItemInput = screen.getByRole('searchbox', {
+    name: /new item name/i,
+  });
+  const addNewItemButton = screen.getByRole('button', {
+    name: /add new item/i,
+  });
+  await user.type(newItemInput, 'Laptop');
+  await user.click(addNewItemButton);
+  expect(screen.getByLabelText('Laptop')).not.toBeChecked();
+});
+
+it('clears the input field after adding a new item', async () => {
+  const { user } = render(<PackingList />);
+  const newItemInput = screen.getByRole('searchbox', {
+    name: /new item name/i,
+  });
+  const addNewItemButton = screen.getByRole('button', {
+    name: /add new item/i,
+  });
+  await user.type(newItemInput, 'Laptop');
+  await user.click(addNewItemButton);
+  expect(newItemInput).toHaveValue('');
+});
+
+it('removes an item from the list when clicking the "Remove" button', async () => {
+  const { user } = render(<PackingList />);
+  const newItemInput = screen.getByRole('searchbox', {
+    name: /new item name/i,
+  });
+  const addNewItemButton = screen.getByRole('button', {
+    name: /add new item/i,
+  });
+  await user.type(newItemInput, 'Laptop');
+  await user.click(addNewItemButton);
+  const removeButton = screen.getByLabelText(/remove laptop/i);
+  await user.click(removeButton);
+  expect(removeButton).not.toBeInTheDocument();
+});
